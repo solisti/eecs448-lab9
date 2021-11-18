e3670ca4e8cd38744c8c45c4f68b01fc6633ec9d
Alice Chen <89803672+solisti@users.noreply.github.com>
2021-11-18 14:32:39 -0600

    Add files via upload

--- multTable.php
+++ multTable.php
@@ -1,15 +1,17 @@
+<style><?php include 'multTable'; ?></style>
 <?php
 echo "<table border=\"1\">";
 echo "<tr>";
-  for ($x=0; $x <= 100 ; $x++) {
-    echo "<td>" . $x . "</td>";
+  echo "<td></td>";
+  for ($x=1; $x <= 100 ; $x++) {
+    echo "<th>" . $x . "</th>";
   }
 echo "</tr>";
 
 for ($i=1; $i <= 100; $i++) {
   echo '<tr>';
 
-  echo "<td>" . $i . "</td>";
+  echo "<th>" . $i . "</th>";
   for ($j=1; $j <= 100; $j++) {
     echo "<td> " . $i * $j . " </td>";
   }
